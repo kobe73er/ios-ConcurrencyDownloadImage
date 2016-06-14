12308a95ce235290ae3034ad874e244eabdbb32c
kobe73er <262103206@qq.com>
2016-06-14 17:14:43 +0800

    using nsoperation queue to do the same

--- ConcurrencyDemo/ViewController.swift
+++ ConcurrencyDemo/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 let imageURLs = ["http://www.planetware.com/photos-large/F/france-paris-eiffel-tower.jpg", "http://adriatic-lines.com/wp-content/uploads/2015/04/canal-of-Venice.jpg", "http://algoos.com/wp-content/uploads/2015/08/ireland-02.jpg", "http://bdo.se/wp-content/uploads/2014/01/Stockholm1.jpg"]
 
+var queue = NSOperationQueue()
+
+
 class Downloader {
     
     class func downloadImageWithURL(url:String) -> UIImage! {
@@ -118,51 +121,94 @@ class ViewController: UIViewController {
     /**
      This version we Using Serial Dispatch Queues
      use the function dispatch_queue_create to create a new queue
+     NOTE:
+     1.It take a bit long to download the images, as compared to the case of concurrent queue. The reason is that we only load one image at a time. Each task waits for the previous task to finish before being executed.
+     2.Images are loaded in order image1, image2, image3, and image4. That’s because the queue is a serial queue that executes one task a time.
     */
+//    @IBAction func didClickOnStart(sender: AnyObject) {
+//        let serialQueue = dispatch_queue_create("com.appcoda.imagesQueue", DISPATCH_QUEUE_SERIAL)
+//        
+//        
+//        dispatch_async(serialQueue) { () -> Void in
+//            
+//            let img1 = Downloader .downloadImageWithURL(imageURLs[0])
+//            dispatch_async(dispatch_get_main_queue(), {
+//                
+//                self.imageView1.image = img1
+//            })
+//            
+//        }
+//        dispatch_async(serialQueue) { () -> Void in
+//            
+//            let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+//            
+//            dispatch_async(dispatch_get_main_queue(), {
+//                
+//                self.imageView2.image = img2
+//            })
+//            
+//        }
+//        dispatch_async(serialQueue) { () -> Void in
+//            
+//            let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+//            
+//            dispatch_async(dispatch_get_main_queue(), {
+//                
+//                self.imageView3.image = img3
+//            })
+//            
+//        }
+//        dispatch_async(serialQueue) { () -> Void in
+//            
+//            let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+//            
+//            dispatch_async(dispatch_get_main_queue(), {
+//                
+//                self.imageView4.image = img4
+//            })
+//        }
+//    }
+    
+    /**
+     using nsoperation queue
+ */
     @IBAction func didClickOnStart(sender: AnyObject) {
-        let serialQueue = dispatch_queue_create("com.appcoda.imagesQueue", DISPATCH_QUEUE_SERIAL)
-        
-        
-        dispatch_async(serialQueue) { () -> Void in
+         queue = NSOperationQueue()
+        queue.addOperationWithBlock{()->Void in
+            let img1 = Downloader.downloadImageWithURL(imageURLs[0])
             
-            let img1 = Downloader .downloadImageWithURL(imageURLs[0])
-            dispatch_async(dispatch_get_main_queue(), {
-                
+            NSOperationQueue.mainQueue().addOperationWithBlock({
                 self.imageView1.image = img1
             })
-            
         }
-        dispatch_async(serialQueue) { () -> Void in
-            
+        
+        queue.addOperationWithBlock { () -> Void in
             let img2 = Downloader.downloadImageWithURL(imageURLs[1])
             
-            dispatch_async(dispatch_get_main_queue(), {
-                
+            NSOperationQueue.mainQueue().addOperationWithBlock({
                 self.imageView2.image = img2
             })
             
         }
-        dispatch_async(serialQueue) { () -> Void in
-            
+        
+        queue.addOperationWithBlock { () -> Void in
             let img3 = Downloader.downloadImageWithURL(imageURLs[2])
             
-            dispatch_async(dispatch_get_main_queue(), {
-                
+            NSOperationQueue.mainQueue().addOperationWithBlock({
                 self.imageView3.image = img3
             })
             
         }
-        dispatch_async(serialQueue) { () -> Void in
-            
+        
+        queue.addOperationWithBlock { () -> Void in
             let img4 = Downloader.downloadImageWithURL(imageURLs[3])
             
-            dispatch_async(dispatch_get_main_queue(), {
-                
+            NSOperationQueue.mainQueue().addOperationWithBlock({
                 self.imageView4.image = img4
             })
+            
         }
     }
-    
-    
+
 }
 
